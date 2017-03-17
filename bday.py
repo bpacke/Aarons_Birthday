2d586924031398692b23dece5f55bbb7851ad44f
Benjamin <bpacke@unm.edu>
2017-03-17 22:57:57 +0000

    factored in the posibility that you may not actually run this on your birthday#

--- bday.py
+++ bday.py
@@ -9,9 +9,13 @@ class Friend:
 
 friends = [Friend('Aaron', date(1993,03,17), 'jBm_6SKAML8'), Friend('Ben', date(1994,04,16), 'RHw50kjFuoA')]
 
-t = date.today()
-
+# t = date.today()
+t = date(2017,03,18)
 for f in friends:
-	if t.day == f.dob.day and t.month == f.dob.month:
+	if t.month == f.dob.month and t.day == f.dob.day:
 		print 'Happy Birthday, %s!' % f.name
 		wb.get().open(f.vid)
+	elif t.month > f.dob.month or (t.month == f.dob.month and t.day > f.dob.day):
+		print '%s, you missed your birthday surprise! Happy belated birthday anyway!' % f.name
+		wb.get().open(f.vid)
+
